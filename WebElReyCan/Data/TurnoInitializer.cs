a6537720bf02253da28b8a250932d1b5dfa70196
Sol Morelli <sol.andrea.morelli@gmail.com>
2021-11-12 11:05:06 -0300

    Add project files.

---
+++ WebElReyCan/Data/TurnoInitializer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebElReyCan.Models;
+
+namespace WebElReyCan.Data
+{
+    public class TurnoInitializer : DropCreateDatabaseAlways<TurnoDbContext>
+    {
+        protected override void Seed(TurnoDbContext context)
+        {
+            var turnos = new List<Turno>
+            {
+                new Turno
+                {
+                    Fecha = DateTime.Now.Day.ToString() + "/" + DateTime.Now.Month.ToString() + "/" + DateTime.Now.Year.ToString(),
+                    Hora = "13:40",
+                    NombreMascota = "Stefano",
+                    Raza = "Mestizo",
+                    Edad = 13,
+                    NombreDuenio = "Mario",
+                    Celular = "15-1111-1111"
+                },
+                 new Turno
+                {
+                    Fecha = "14/11/2021",
+                    Hora = "16:30",
+                    NombreMascota = "Turi",
+                    Raza = "Mestizo",
+                    Edad = 5,
+                    NombreDuenio = "Almendra",
+                    Celular = "15-2222-2222"
+                },
+                  new Turno
+                {
+                    Fecha = DateTime.Now.Day.ToString() + "/" + DateTime.Now.Month.ToString() + "/" + DateTime.Now.Year.ToString(),
+                    Hora = "17:30",
+                    NombreMascota = "Piltrafa",
+                    Raza = "Atigrado",
+                    Edad = 8,
+                    NombreDuenio = "Esmé",
+                    Celular = "15-3333-3333"
+                },
+                   new Turno
+                {
+                    Fecha = "13/11/2021",
+                    Hora = "15:10",
+                    NombreMascota = "Kelo",
+                    Raza = "Mestizo",
+                    Edad = 10,
+                    NombreDuenio = "Aurora",
+                    Celular = "15-4444-4444"
+                },
+                    new Turno
+                {
+                    Fecha = "13/11/2021",
+                    Hora = "14:10",
+                    NombreMascota = "Bebito",
+                    Raza = "Mestizo",
+                    Edad = 1,
+                    NombreDuenio = "Diana",
+                    Celular = "15-5555-5555"
+                }
+            };
+
+            turnos.ForEach( turno => context.Turnos.Add(turno) );
+            context.SaveChanges();
+        }
+    }
+
+}
---
+++ WebElReyCan/Models/Turno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebElReyCan.Models
+{
+    [Table("Turno")]
+    public class Turno
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Column(TypeName = "varchar")]
+        [StringLength(50, ErrorMessage = "Este campo tiene un máximo de 50 caracteres.")]
+        public string Fecha { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
+        [Column(TypeName = "varchar")]
+        [StringLength(50, ErrorMessage = "Este campo tiene un máximo de 50 caracteres.")]
+        public string Hora { get; set; }
+
+        [DisplayName("Nombre Mascota")]
+        [Required(ErrorMessage = "Campo requerido.")]
+        [Column(TypeName = "varchar")]
+        [StringLength(50, ErrorMessage = "Este campo tiene un máximo de 50 caracteres.")]
+        public string NombreMascota { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(50, ErrorMessage = "Este campo tiene un máximo de 50 caracteres.")]
+        public string Raza { get; set; }
+
+        [Column(TypeName = "int")]
+        public int Edad { get; set; }
+
+        [DisplayName("Nombre Dueño")]
+        [Required(ErrorMessage = "Campo requerido.")]
+        [Column(TypeName = "varchar")]
+        [StringLength(50, ErrorMessage = "Este campo tiene un máximo de 50 caracteres.")]
+        public string NombreDuenio { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido.")]
+        [Column(TypeName = "varchar")]
+        [StringLength(50, ErrorMessage = "Este campo tiene un máximo de 50 caracteres.")]
+        public string Celular { get; set; }
+    }
+}
