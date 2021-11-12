81246fcdc5042031193fec4998ac4ec89911bf4d
Sol Morelli <sol.andrea.morelli@gmail.com>
2021-11-12 12:22:29 -0300

    ABML y otros

--- WebElReyCan/Controllers/TurnoController.cs
+++ WebElReyCan/Controllers/TurnoController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -51,5 +52,90 @@ namespace WebElReyCan.Controllers
             return View("Create", turno);
         }
 
+        [HttpGet] // /Turno/Delete
+        public ActionResult Delete(int id)
+        {
+            Turno turno = context.Turnos.Find(id);
+
+            if (turno != null)
+            {
+                return View("Delete", turno);
+            }
+
+            return HttpNotFound();
+        }
+
+        [HttpPost] // /Turno/Delete
+        public ActionResult Delete(Turno turno)
+        {
+            context.Turnos.Remove(context.Turnos.Find(turno.Id));
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet] // /Turno/Edit
+        public ActionResult Edit(int id)
+        {
+            Turno turno = context.Turnos.Find(id);
+
+            if (turno != null)
+            {
+                return View("Edit", turno);
+            }
+
+            return HttpNotFound();
+        }
+
+        [HttpPost] // /Turno/Edit
+        public ActionResult Edit(Turno turno)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry( context.Turnos.Find(turno.Id) ).State = EntityState.Detached;
+                context.Entry(turno).State = EntityState.Modified;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View("Edit", turno);
+        }
+
+        [HttpGet] // /Turno/GetByName
+        public ActionResult GetByName(string name)
+        {
+            List<Turno> turnos =
+            (
+                from turno in context.Turnos
+                where turno.NombreMascota == name
+                select turno
+            ).ToList<Turno>();
+
+            if (turnos != null)
+            {
+                return View("Index", turnos);
+            }
+
+            return HttpNotFound();
+        }
+
+        [HttpGet] // /Turno/GetByCellphone
+        public ActionResult GetByCellphone(string cellphone)
+        {
+            List<Turno> turnos =
+            (
+                from turno in context.Turnos
+                where turno.Celular == cellphone
+                select turno
+            ).ToList<Turno>();
+
+            if (turnos != null)
+            {
+                return View("Index", turnos);
+            }
+
+            return HttpNotFound();
+        }
     }
 }
